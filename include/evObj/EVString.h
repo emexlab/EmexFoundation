a3dff8dfc5f1c6c5d1e99723c0fd9284bc629841
mach-port-t <fridakitten@pm.me>
2026-06-27 07:07:36 +0200

    feat: forgot to add the header

---
+++ include/evObj/EVString.h
@@ -0,0 +1,51 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 emexLabs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef EVSTRING_H
+#define EVSTRING_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <evObj/runtime/EVBase.h>
+
+typedef enum: uint8_t {
+    kEVStringEncodingUTF7,
+    kEVStringEncodingASCII = kEVStringEncodingUTF7,
+    kEVStringEncodingUTF8,
+} kEVStringEncoding;
+
+typedef EVObjectRef EVStringRef;
+
+EVTypeID EVStringGetTypeID(void);
+
+EVStringRef EVStringCreateWithCString(EVAllocator *allocator, const char *str, kEVStringEncoding encoding);
+EVStringRef EVStringCreateWithCStringNoCopy(EVAllocator *allocator, const char *str, kEVStringEncoding encoding);
+
+EVStringRef EVStringCreateCopy(EVAllocator *allocator, EVStringRef stringRef);
+
+const char *EVStringGetCStringPtr(EVStringRef stringRef, kEVStringEncoding encoding);
+size_t EVStringGetLength(EVStringRef stringRef);
+bool EVStringGetCString(EVStringRef stringRef, char *str, size_t str_len, kEVStringEncoding encoding);
+
+#endif /* EVSTRING_H */
