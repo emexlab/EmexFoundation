4b72744ea1385184400b0dbda9963bf1301d417b
mach-port-t <fridakitten@pm.me>
2026-07-16 23:53:59 +0200

    fix: marking EFAUTOTRANSFER as __extension__

--- include/EmexFoundation/runtime/EFRuntime.h
+++ include/EmexFoundation/runtime/EFRuntime.h
@@ -32,11 +32,12 @@
 
 /* releases the object when it's out of scope */
 #define EFAUTOREL __attribute__((cleanup(EFReleaseTryHelper)))
-#define EFAUTOTRANSFER(var) ({ \
-    __typeof__(var) _temp = (var); \
-    (var) = NULL; \
-    _temp; \
-})
+#define EFAUTOTRANSFER(var) \
+    __extension__ ({ \
+        __typeof__(var) _temp = (var); \
+        (var) = NULL; \
+        _temp; \
+    })
 
 EFTypeID EFClassRegister(EFClass *cls);
 EFClass *EFClassGetByID(EFTypeID id);
